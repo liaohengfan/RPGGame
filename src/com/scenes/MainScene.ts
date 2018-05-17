8a7257619b531d6bbf1282b460d202549e688d9b
liaohengfan <liaohengfan@yeah.net>
2018-05-17 15:42:10 +0800

    add scene models

--- src/com/scenes/MainScene.ts
+++ src/com/scenes/MainScene.ts
@@ -24,36 +24,38 @@ class MainScene extends Scene implements ILHFScene{
     }
 
     private initCitys():void{
-        //this.citys=this.resourcesMana.getModels('luoyang');
-        //this.citys=this.resourcesMana.getModels('daming');
         this.citys=this.resourcesMana.getModels('xingxiu');
-        //console.log(this.citys.getObjectByName('daguan_6_mesh_0_110267'));
-        //console.log(this.citys);
-        let guanmu:Mesh=this.citys.getObjectByName('daguan_6_mesh_0_110267') as Mesh;
+        /*let guanmu:Mesh=this.citys.getObjectByName('daguan_6_mesh_0_110207') as Mesh;
+        console.log('citys:',this.citys);
         if(guanmu){
             let p:MeshPhongMaterial=guanmu.material as MeshPhongMaterial;
+            console.log('guanmu:',guanmu);
             if(p){
                 p.transparent=true;
-                p.alphaMap=p.map;
+                //p.alphaMap=p.map;
             }
-        }
-        /*this.citys.traverse((obj:Object3D)=>{
-            let mesh:Mesh=(obj as Mesh);
-            if(mesh){
-                let material:Material=(mesh.material as Material);
-                if(material) {
-                    //material.side = DoubleSide;
-                    material.transparent=true;
+        }*/
+        this.citys.traverse(function(obj:Object3D){
+
+            let guanmu:Mesh=obj as Mesh;
+            if(guanmu){
+                let p:MeshPhongMaterial=guanmu.material as MeshPhongMaterial;
+                console.log('guanmu:',guanmu);
+                if(p){
+                    p.transparent=true;
+                    //p.alphaMap=p.map;
                 }
             }
-        });*/
+
+        });
+
         this.add(this.citys);
     }
 
     private initEnv():void{
         this.add(new AxesHelper(1000));
         this.add(new AmbientLight(0xFFFFFF,1));
-        let skyGeo:SphereBufferGeometry=new SphereBufferGeometry(10000,32,16);
+        let skyGeo:SphereBufferGeometry=new SphereBufferGeometry(100000,32,16);
         let skyMaterial:MeshBasicMaterial=new MeshBasicMaterial({
             color:0xFFFFFF,
             side:BackSide,
